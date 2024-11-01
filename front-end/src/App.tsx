93a2ec3aec8fd15480df36e8ac0dc130db109ae2
Gokuleshwaran Narayanan <gokulnpc@gmail.com>
2024-11-01 02:24:42 -0400

    bugs

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,5 @@
 import "@rainbow-me/rainbowkit/styles.css";
 
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
